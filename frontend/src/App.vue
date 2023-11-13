9f4d033daa9722b7241580bf8fa161f1772fbfa5
Kinato <jromain040@gmail.com>
2023-11-13 14:36:25 +0100

    Crypto Accueil

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <body>
-    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
-      <Menu />
+    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" id="app">
+      <Sidebar :isSideMenuOpen="isSideMenuOpen" @close-side-menu="isSideMenuOpen = false" />
       <div class="flex flex-col flex-1 w-full">
-        <Header />
-        <Accueil />
+        <Header @open-side-menu="isSideMenuOpen = !isSideMenuOpen" />
+        <div class="p-6 dark:text-white">
+          <RouterView />
+        </div>
       </div>
     </div>
   </body>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Menu from './components/Menu.vue'
+import { ref } from "vue";
+
+import { RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar.vue'
 import Header from './components/Header.vue'
-import Accueil from './views/Accueil.vue'
+
+const isSideMenuOpen = ref(false);
 </script>
 
 <style scoped></style>
---
+++ frontend/src/components/ListeCrypto.vue
@@ -0,0 +1,41 @@
+<template>
+  <tr class="text-gray-700 dark:text-gray-400">
+    <td class="px-4 py-3 text-sm">1</td>
+    <td class="px-4 py-3 text-sm flex">
+      <div class="w-10 h-10 mr-3 rounded-full md:block">
+        <img :src="crypto.img" :alt="crypto.img"
+          class="AssetLogo__ImageStyled-sc-1m18p9x-0 itlbLb ui mini right spaced middle aligned image" />
+      </div>
+      <div style="display: inline-block; vertical-align: middle" class="ellipsis">
+        <a href="/assets/bitcoin">{{ crypto.nom }}
+          <p style="font-size: 0.8em; opacity: 0.7">{{ crypto.accronyme }}</p>
+        </a>
+      </div>
+    </td>
+    <td class="px-4 py-3 text-xs">{{ crypto.prix }}€</td>
+    <td class="px-4 py-3 text-sm">{{ crypto.capitalisation }}€</td>
+    <td class="px-4 py-3 text-sm">{{ crypto.vwap }}€</td>
+    <td class="px-4 py-3 text-sm">{{ crypto.volume }}€</td>
+    <td class="px-4 py-3 text-xs">
+      <span
+        class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100" v-if="crypto.typefluc == 'positive'">
+        +{{ crypto.fluctuation }}%
+      </span>
+      <span
+        class="px-2 py-1 font-semibold leading-tight text-red-500 bg-red-100 rounded-full dark:bg-red-700 dark:text-red-100" v-if="crypto.typefluc == 'negative'">
+        -{{ crypto.fluctuation }}%
+      </span>
+    </td>
+  </tr>
+</template>
+
+<script setup>
+const { crypto } = defineProps({
+  crypto: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style></style>
