5dcd46d77d87586e0d358ed05e165f343767238e
Kinato <jromain040@gmail.com>
2023-11-14 17:49:54 +0100

    bouton connexion et inscription

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -46,8 +46,8 @@ const pages = ref([
   {
     img:
       "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01",
-    to: "/echange",
-    name: "Echange",
+    to: "/exchange",
+    name: "Exchange",
   },
 ]);
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Accueil from '../views/Accueil.vue'
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import Exchange from "../views/Exchange.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,10 @@ const router = createRouter({
       path: '/register',
       name: 'Register',
       component: Register
+    }, {
+      path: '/exchange',
+      name: 'Exchange',
+      component: Exchange
     }
   ]
 })
