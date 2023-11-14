727cc94f4a916aa101d0848731a155fce7379374
Andréas Cottet <andrecot2502@gmail.com>
2023-11-14 17:57:52 +0100

    WIP

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:3000'
+const API_CRYPTO_URL = 'https://api.coincap.io/v2'
 
 export async function login(username, password) {
     return await axios.post(API_URL + '/login', {})
+}
+
+export async function getCryptos() {
+    return await axios.get(API_CRYPTO_URL + '/assets', )
 }
