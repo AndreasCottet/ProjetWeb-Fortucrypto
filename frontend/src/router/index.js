1ead661df8c582e6451c5cc14e463cc2c58ddb2c
Andréas Cottet <andrecot2502@gmail.com>
2023-11-14 17:33:56 +0100

    WIP

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Accueil from '../views/Accueil.vue'
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <form class="flex flex-col rounded-lg border-2 border-gray-500 bg-gray-800 w-fit py-4 px-8 mx-auto mt-20" @submit.prevent="register">
-    <h1 class="font-bold text-lg my-4">S'inscrire'</h1>
+    <h1 class="font-bold text-lg my-4">S'inscrire</h1>
 
     <label class="mb-1 font-light">Nom d'utilisateur</label>
     <input type="text" placeholder="Michel Michel" class="w-96 bg-gray-500 rounded-lg px-4 py-2 mb-4" v-model="form.username">
